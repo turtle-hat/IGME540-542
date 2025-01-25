02dd9d7e725a4b1d3f965a5b2337e64167a04832
turtle-hat <org7993@rit.edu>
2025-01-24 19:47:50 -0500

    A01: Updated starter code to D3D12

--- Game.h
+++ Game.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <d3d11.h>
+#include <d3d12.h>
 #include <wrl/client.h>
 
 class Game
@@ -21,7 +21,7 @@ public:
 private:
 
 	// Initialization helper methods - feel free to customize, combine, remove, etc.
-	void LoadShaders();
+	void CreateRootSigAndPipelineState();
 	void CreateGeometry();
 
 	// Note the usage of ComPtr below
@@ -29,13 +29,19 @@ private:
 	//     Component Object Model, which DirectX objects do
 	//  - More info here: https://github.com/Microsoft/DirectXTK/wiki/ComPtr
 
-	// Buffers to hold actual geometry data
-	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
+	// Pipeline
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState;
+	
+	// Geometry
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW vbView{};
 
-	// Shaders and shader-related constructs
-	Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader;
-	Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader;
-	Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout;
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexBuffer;
+	D3D12_INDEX_BUFFER_VIEW ibView{};
+
+	// Other graphics data
+	D3D12_VIEWPORT viewport{};
+	D3D12_RECT scissorRect{};
 };
 
