3552840fd40e941de77a642edd3c962f2cb63952
turtle-hat <org7993@rit.edu>
2025-02-10 16:43:31 -0500

    A03: Finished Material class

--- Material.cpp
+++ Material.cpp
@@ -1,4 +1,5 @@
 #include "Material.h"
+#include "Graphics.h"
 
 Material::Material(Microsoft::WRL::ComPtr<ID3D12PipelineState> _pso, DirectX::XMFLOAT3 _colorTint, DirectX::XMFLOAT2 _uvScale, DirectX::XMFLOAT2 _uvOffset)
 {
@@ -18,35 +19,59 @@ D3D12_GPU_DESCRIPTOR_HANDLE Material::GetFinalGPUHandleForSRVs()
 
 DirectX::XMFLOAT3 Material::GetColorTint()
 {
-	return DirectX::XMFLOAT3();
+	return colorTint;
 }
 
 DirectX::XMFLOAT2 Material::GetUVScale()
 {
-	return DirectX::XMFLOAT2();
+	return uvScale;
 }
 
 DirectX::XMFLOAT2 Material::GetUVOffset()
 {
-	return DirectX::XMFLOAT2();
+	return uvOffset;
 }
 
 void Material::SetColorTint(DirectX::XMFLOAT3 _colorTint)
 {
+	colorTint = _colorTint;
 }
 
 void Material::SetUVScale(DirectX::XMFLOAT2 _uvScale)
 {
+	uvScale = _uvScale;
 }
 
 void Material::SetUVOffset(DirectX::XMFLOAT2 _uvOffset)
 {
+	uvOffset = _uvOffset;
 }
 
+// Adds a texture SRV to the material to be referred to by a specific texture register
 void Material::AddTexture(D3D12_CPU_DESCRIPTOR_HANDLE _srv, int _slot)
 {
+	// Don't continue if already finalized
+	if (finalized) return;
+
+	textureSRVsBySlot[_slot] = _srv;
+	// If this is the highest texture slot the material has had assigned, set the marker accordingly
+	highestTextureSlotInUse = max(highestTextureSlotInUse, _slot);
 }
 
 void Material::FinalizeMaterial()
 {
+	// Don't continue if already finalized
+	if (finalized) return;
+
+	// Copy each texture SRV to the GPU
+	for (unsigned int i = 0; i <= highestTextureSlotInUse; i++) {
+		D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle = Graphics::CopySRVsToDescriptorHeapAndGetGPUDescriptorHandle(textureSRVsBySlot[i], 1);
+		
+		// Save GPU handle of first texture
+		if (i == 0) {
+			finalGPUHandleForSRVs = gpuHandle;
+		}
+	}
+
+	finalized = true;
 }
